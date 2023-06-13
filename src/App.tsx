99828ca7cc34cd7e2d3d1f86b28befec32c03bc0
Clizio Guedes <clizioguedes@ioasys.com.br>
2023-06-13 09:04:50 -0300

    feature: added global application styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
@@ -7,6 +8,7 @@ export function App() {
       <div>
         <h1>Hello Word</h1>
       </div>
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
